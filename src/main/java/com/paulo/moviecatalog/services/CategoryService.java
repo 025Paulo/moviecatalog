4336e2966df5089eedcaeb79ba4a302bf6d3dd8c
Paulo Batista <paulo.batista025@outlook.comgit config --global user.email paulo.batista025@outlook.comgit config --global user.email paulo.batista025@outlook.com>
2025-02-10 20:52:56 -0300

    Find by Id, exception handler

--- src/main/java/com/paulo/moviecatalog/services/CategoryService.java
+++ src/main/java/com/paulo/moviecatalog/services/CategoryService.java
@@ -3,12 +3,13 @@ package com.paulo.moviecatalog.services;
 import com.paulo.moviecatalog.dto.CategoryDTO;
 import com.paulo.moviecatalog.entities.Category;
 import com.paulo.moviecatalog.repositories.CategoryRepository;
+import com.paulo.moviecatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -25,4 +26,10 @@ public class CategoryService {
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+        return new CategoryDTO(entity);
+    }
 }
